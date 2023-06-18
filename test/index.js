800d13597f44c56c046802354e59664dfa7a12ea
Freddie Gilbraith <freddie.gilbraith@littlebonsai.co.uk>
2023-06-18 16:15:41 +0100

    wip 2023-06-18_16:15:41

--- test/index.js
+++ test/index.js
@@ -221,7 +221,8 @@ t.test("examples", (t) => {
   const test = runTest.bind(null, t);
   test(
     "function calls and diverts",
-    `=== armoury_1 ===
+    `
+	  === armoury_1 ===
 * George: Hello, fellow Hentai Heads -> map_2
 * Jerry: George! You got in to Spooky High too?
 Kramer: Dang, George has used up all the hentai here, we'll have to look elsewhere!
