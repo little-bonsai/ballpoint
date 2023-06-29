2708c51a552e1d2d50ad692ed24a13672b33e85d
Freddie Gilbraith <freddie.gilbraith@littlebonsai.co.uk>
2023-06-29 10:19:22 +0100

    feat: add validation to cli

--- index.js
+++ index.js
@@ -7,58 +7,103 @@ require("inkjs/compiler/Parser/InkParser");
 require("inkjs/compiler/parser/StatementLevel");
 
 const format = require("./lib/format");
+const validate = require("./lib/validate");
+
+function doValidation(src, out, inputFilename) {
+	const validation = validate(src, out, inputFilename);
+
+	if (validation) {
+		process.stdout.write(" [ Validation Error ]\n");
+		const lineNumber = Number(validation.message.match(/\d+/));
+		console.log(validation);
+		console.log("");
+		console.log(
+			out
+				.split("\n")
+				.flatMap((line, i, { length }) => {
+					const lineMarker = String(i + 1).padEnd(
+						1 + Math.ceil(Math.log10(length)),
+						" "
+					);
+
+					return i === lineNumber - 1
+						? [
+								lineMarker + line,
+
+								new Array(1 + Math.ceil(Math.log10(length)))
+									.fill(" ")
+									.join("") +
+									new Array(line.length).fill("^").join(""),
+						  ]
+						: lineMarker + line;
+				})
+				.slice(Math.max(0, lineNumber - 3), lineNumber + 4)
+				.join("\n")
+		);
+	}
+
+	return validation;
+}
 
 async function main(args) {
-  if (args["--help"]) {
-    return printHelp();
-  }
+	if (args["--help"]) {
+		return printHelp();
+	}
+	if (args["--verbose"]) {
+		console.log({ args });
+		console.log("");
+	}
 
-  for (const inputFilename of args._) {
-    process.stdout.write(inputFilename);
+	for (const inputFilename of args._) {
+		process.stdout.write(inputFilename);
 
-    const src = await fs.readFile(inputFilename, "utf8");
-    const out = (() => {
-      try {
-        return format(src, inputFilename);
-      } catch (e) {
-        return e;
-      }
-    })();
+		const src = await fs.readFile(inputFilename, "utf8");
+		const out = (() => {
+			try {
+				return format(src, inputFilename);
+			} catch (e) {
+				return e;
+			}
+		})();
 
-    if (typeof out === "string") {
-      process.stdout.write(" [ OK ]\n");
+		if (doValidation(src, out, inputFilename)) {
+			break;
+		}
 
-      if (args["--write"]) {
-        await fs.writeFile(inputFilename, out);
-      } else {
-        console.log(out);
-      }
-    } else {
-      process.stdout.write(" [ Error ]\n");
-      console.log(out);
-      break;
-    }
-  }
+		if (typeof out === "string") {
+			process.stdout.write(" [ OK ]\n");
+
+			if (args["--write"]) {
+				await fs.writeFile(inputFilename, out);
+			} else {
+				console.log(out);
+			}
+		} else {
+			process.stdout.write(" [ Format Error ]\n");
+			console.log(out);
+			break;
+		}
+	}
 }
 
 const argSpec = {
-  // General
-  "--help": Boolean,
-  "--version": Boolean,
-  "--verbose": arg.COUNT, // Counts the number of times --verbose is passed
+	// General
+	"--help": Boolean,
+	"--version": Boolean,
+	"--verbose": arg.COUNT, // Counts the number of times --verbose is passed
 
-  "--sort-includes": Boolean,
-  "--write": Boolean,
+	"--sort-includes": Boolean,
+	"--write": Boolean,
 
-  // Aliases
-  "-h": "--help",
-  "-w": "--write",
-  "-v": "--verbose",
+	// Aliases
+	"-h": "--help",
+	"-w": "--write",
+	"-v": "--verbose",
 };
 
 function printHelp() {
-  console.log(
-    `
+	console.log(
+		`
 Ballpoint
 A CLI for formatting .ink files
 
@@ -79,8 +124,8 @@ Arguments:
         -s = --sort-includes
         -v = --verbose
 `.trim(),
-    "\n"
-  );
+		"\n"
+	);
 }
 
 main(arg(argSpec, { permissive: true, argv: process.argv.slice(2) }));
